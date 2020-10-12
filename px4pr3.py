2f3a57cb8d62ecd01f9a5dcd6f16ac268d7febc8
wedobecoding <72720563+wedobecoding@users.noreply.github.com>
2020-10-11 23:14:18 -0400

    Add files via upload

---
+++ px4pr3.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 11 20:44:18 2020
+
+@author: marisadobbins
+"""
+
+def bitwise_and(b1,b2):
+    if len (b1) == 0 :
+        bitwise_rest == bitwise_and(b1[:-1, b2[:-1]])
+        return bitwise_rest + bitwise_and(b1)
+    
+       return ''
+    elif len (b1) > 8 or len(b2) >8
+        bitwise_rest == bitwise_and(b1[:-1, b2[:-1]])
+        if len(b1)>8
+    else:
+        
+        return b1[-1] AND b2[-1] + bitwise_rest
+    
